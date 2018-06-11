9fdb5c9839095d79c6a547d61430f0953b08900b
adams16 <34104236+adams16@users.noreply.github.com>
2018-06-12 01:00:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/adams/shoppershub/SigninActivity.java
@@ -0,0 +1,107 @@
+package com.example.adams.shoppershub;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonsignin;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private TextView textViewsignup;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        setTitle("SignIn");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            Intent intent3 = new Intent(SigninActivity.this, SendCodeActivity.class);
+            startActivity(intent3);
+        }
+
+        progressDialog = new ProgressDialog(this);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonsignin = (Button) findViewById(R.id.buttonsignin);
+
+        textViewsignup = (TextView) findViewById(R.id.textViewsignup);
+
+        buttonsignin.setOnClickListener(this);
+        textViewsignup.setOnClickListener(this);
+    }
+
+    private void UserLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (editTextEmail.getText().length() == 0) {
+            editTextEmail.setError("Please Enter the Email");
+            return;
+        }
+        if (editTextPassword.getText().length() == 0) {
+            editTextPassword.setError("Please Enter the Password");
+            return;
+        }
+        progressDialog.setMessage("Signing In...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            Intent intent2 = new Intent(SigninActivity.this, SendCodeActivity.class);
+                            startActivity(intent2);
+                            finish();
+                        } else {
+                            Toast.makeText(SigninActivity.this, "SignIn Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonsignin)
+        {
+            UserLogin();
+        }
+        if (view == textViewsignup)
+        {
+            Toast.makeText(getApplicationContext(), "Create New Account", Toast.LENGTH_SHORT).show();
+            Intent intent1 = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivity(intent1);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/adams/shoppershub/SignupActivity.java
@@ -0,0 +1,132 @@
+package com.example.adams.shoppershub;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonsignup;
+
+    private EditText editTextName1;
+    private EditText editTextEmail1;
+    private EditText editTextPassword1;
+    private EditText editTextContact1;
+
+    private TextView textViewsignin;
+
+    private ProgressDialog progressDialog1;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        setTitle("SignUp");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null)
+        {
+            finish();
+            Intent intent2 = new Intent(SignupActivity.this,SendCodeActivity.class);
+            startActivity(intent2);
+        }
+
+        progressDialog1 = new ProgressDialog(this);
+
+        editTextName1 = (EditText) findViewById(R.id.editTextName);
+        editTextEmail1 = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword1 = (EditText) findViewById(R.id.editTextPassword);
+        editTextContact1 = (EditText) findViewById(R.id.editTextContact);
+
+
+        buttonsignup = (Button) findViewById(R.id.buttonsignup);
+
+        textViewsignin = (TextView) findViewById(R.id.textViewsignin);
+
+        buttonsignup.setOnClickListener(this);
+        textViewsignin.setOnClickListener(this);
+    }
+
+    private void registerUser()
+    {
+        String name = editTextName1.getText().toString().trim();
+        String email = editTextEmail1.getText().toString().trim();
+        String password = editTextPassword1.getText().toString().trim();
+        String contact = editTextContact1.getText().toString().trim();
+
+        if (editTextName1.getText().length() == 0)
+        {
+            editTextName1.setError("Please Enter the Name");
+            return;
+        }
+        if (editTextEmail1.getText().length() == 0)
+        {
+            editTextEmail1.setError("Please Enter the Email");
+            return;
+        }
+        if (editTextContact1.getText().length() == 0)
+        {
+            editTextContact1.setError("Please Enter the Contact Number");
+            return;
+        }
+        if (editTextPassword1.getText().length() == 0)
+        {
+            editTextPassword1.setError("Please Enter the Password");
+            return;
+        }
+
+
+        progressDialog1.setMessage("Registering User...");
+        progressDialog1.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SignupActivity.this, "Registration Complete",Toast.LENGTH_SHORT).show();
+                            Intent intent1 = new Intent(SignupActivity.this,SendCodeActivity.class);
+                            startActivity(intent1);
+
+                        }
+                        else
+                        {
+                            Toast.makeText(SignupActivity.this, "Registration Failed",Toast.LENGTH_SHORT).show();
+                        }
+                        progressDialog1.dismiss();
+                    }
+
+
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonsignup)
+        {
+            registerUser();
+        }
+        if (view == textViewsignin)
+        {
+            Toast.makeText(getApplicationContext(), "Login to Continue", Toast.LENGTH_SHORT).show();
+            Intent intent1 = new Intent(getApplicationContext(), SigninActivity.class);
+            startActivity(intent1);
+        }
+    }
+}
